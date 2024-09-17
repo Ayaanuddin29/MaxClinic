5acda146db553989657be103f718817849ff6fee
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-17 13:10:05 +0530

    api signup

--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoute from './routes/user.route.js'
-dotenv.config()
+import authRoutes from './routes/auth.route.js'
+dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('mongoDB is Connected')
 }).catch(()=>{
     console.log('error')
 })
 const app=express();
+app.use(express.json())
 app.listen(3000,()=>{
     console.log('Server is Running at 3000!!')
 })
 app.use('/api/user',userRoute);
+app.use('/api/auth',authRoutes)

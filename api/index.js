297e171345ab3e95e8ea865e3b5e7946fe5cfe7e
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-17 11:38:44 +0530

    connect db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('mongoDB is Connected')
+}).catch(()=>{
+    console.log('error')
+})
 const app=express();
 app.listen(3000,()=>{
     console.log('Server is Running at 3000!!')

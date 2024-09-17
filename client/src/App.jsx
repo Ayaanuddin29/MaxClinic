6ee9edc3ea2bb885e1d1917c06ff65d846ffc5a5
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-17 11:05:56 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header'
 function App() {
+  const isAuthenticated=false;
   return (
-    <div>
-      <h1 className='text-3xl text-red-500'>Hii</h1>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+
   )
 }
 
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      <div>
+      Welcome to the Max Clinic 
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}

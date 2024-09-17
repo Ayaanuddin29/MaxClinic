92c23ee84fe06ab8e0c0afb7726ae00d39651343
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-17 13:23:07 +0530

    middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-export const signup=async(req,res)=>{
+import { errorHandler } from "../utils/error.js";
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     if(!username||!email||!password||username===''||password===''||email===''){
-        return res.status(400).json({message:'All fields are required'})
+        next(errorHandler(400,'All fields are required'))
     }
     const hashPassword=bcryptjs.hashSync(password,10);
     const newUser=new User({
@@ -16,7 +17,7 @@ export const signup=async(req,res)=>{
         res.json({message:'Successful signup'})
     }
     catch(err){
-        res.status(500).json({message:err})
+        next(err)
     }
     
     
